28a53073cf316494e4eea8d0ec4325bbf9b62208
kananrahimli <kenanrehimli48@gmail.com>
2021-09-27 14:11:56 +0400

    last

--- js/index.js
+++ js/index.js
@@ -152,7 +152,7 @@ $(document).ready(function () {
         $(card).click(function(e){
            
             history.pushState({}, null, 'detail.html');
-            $('.main').html(` <div class="services g-container ">
+            $('.main').html(` <div class="services g-container mb-5 ">
             <div class="services-container d-flex justify-content-between w-100 mt-2 mb-5">
                 <h2 class="services-container__title text-white">
                     <i class="fab fa-ups"></i> Details
